9cd418c42a256702e17fe2c2e89dbec59049a380
montecruiseto <samir.bekaert@gmail.com>
2015-01-30 14:22:21 +0700

    Refactor base setup

--- packages/telescope-theme-sm-gemini/package.js
+++ packages/telescope-theme-sm-gemini/package.js
@@ -10,6 +10,7 @@ Package.onUse(function (api) {
     [
       'telescope-lib',
       'telescope-base',
+      'telescope-theme-hubble',
       'fourseven:scss'
     ],
     ['client', 'server']
@@ -22,27 +23,17 @@ Package.onUse(function (api) {
   api.addFiles(
     [
       // globals
-      // 'lib/client/scss/global/_forms.scss',
-      // 'lib/client/scss/global/_links.scss',
+      'lib/client/scss/global/_forms.scss',
+      'lib/client/scss/global/_links.scss',
 
       // modules
-      'lib/client/scss/modules/_accounts.scss',
-      'lib/client/scss/modules/_banners.scss',
-      'lib/client/scss/modules/_comments.scss',
-      'lib/client/scss/modules/_dialogs.scss',
-      'lib/client/scss/modules/_errors.scss',
       'lib/client/scss/modules/_nav.scss',
-      // 'lib/client/scss/modules/_mobile_nav.scss',
+      'lib/client/scss/modules/_mobile_nav.scss',
       'lib/client/scss/modules/_posts.scss',
-      'lib/client/scss/modules/_share.scss',
-      'lib/client/scss/modules/_user-profile.scss',
 
       // partials
       'lib/client/scss/partials/_colors.scss',
       'lib/client/scss/partials/_grid.scss',
-      'lib/client/scss/partials/_icons.scss',
-      'lib/client/scss/partials/_mixins.scss',
-      'lib/client/scss/partials/_tooltips.scss',
       'lib/client/scss/partials/_typography.scss',
 
       // screen
